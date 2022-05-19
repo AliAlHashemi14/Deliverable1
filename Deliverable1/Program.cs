35c510be7b0d8921ca6ad278b77e3ed9d81dc852
AliAlHashemi14 <alialhashemi14@gmail.com>
2022-05-19 18:27:07 -0400

    Deliverable1

---
+++ Deliverable1/Program.cs
@@ -0,0 +1,26 @@
+﻿var response = "";
+
+do {
+    Console.WriteLine("How many people are we making PB and J sandwiches for?");
+    var peopleCount = Int32.Parse(Console.ReadLine());
+    double totalSlicesNeeded = peopleCount * 2;
+    double pbTbspNeeded = peopleCount * 2;
+    double jellyTbspNeeded = peopleCount * 4;
+    double slicesPerLoaf = 28;
+    double pbPerJar = 32;
+    double jellyPerJar = 48;
+
+    double totalLoavesNeeded = Math.Ceiling(totalSlicesNeeded / slicesPerLoaf);
+    double totalJarPbNeeded = Math.Ceiling(pbTbspNeeded / pbPerJar);
+    double totalJarJellyNeeded = Math.Ceiling(jellyTbspNeeded / jellyPerJar);
+
+    Console.WriteLine($"You need: \n{totalSlicesNeeded} slices of bread \n{pbTbspNeeded} tablespoons of peanut butter \n{jellyTbspNeeded} tablespoons of jelly");
+    Console.WriteLine($"Which is: \n{totalLoavesNeeded} loaves of bread \n{totalJarPbNeeded} jars of peanut butter \n{totalJarJellyNeeded} jars of jelly");
+    Console.WriteLine("Would you like to restart? Enter yes or y to continue, or enter any other key to exit");
+    response = Console.ReadLine();
+    }
+while (response == "y" || response == "yes");
+
+Console.WriteLine("Goodbye!");
+
+
